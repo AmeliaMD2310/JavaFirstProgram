3628fd37b88a43a41a46216e1ec656068adb2d2f
AmeliaMD2310 <w0467922@nscc.ca>
2025-01-19 20:27:25 -0400

    Add files via upload
    Exercise 2 Java Program

---
+++ Car.java
@@ -0,0 +1,31 @@
+public class Car {
+    // add attributes
+    private String name;
+    private int  model;
+    private  String color;
+    
+    
+    
+    // add behaviour  getters and setters (methods)
+        //getters
+        public String getName () {
+           return name; 
+        }
+    
+        public int getModel () {
+           return model; 
+        }
+        public String getColor () {
+           return color; 
+        }
+       // setters
+       public void setName (String n) {
+           this.name = n; 
+        }
+       public void setModel (int m) {
+          this.model = m; 
+        } 
+    public void setColor (String c) {
+            this.color = c;
+        } 
+    }
--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+/*
+Amelia MacDonald
+W0467922
+January 15, 2025
+Exercise 2 
+*/
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello, World!");
+System.out.println("Hello, World!");
+System.out.println("I Love Cars!");
+
+    Car carOne = new Car();
+    System.out.println(carOne.getName () );
+
+    carOne.setName("Dodge Challenger");
+    System.out.println(carOne.getName () );
+
+    carOne.setName("Mitsubishi Lancer")  ;
+    System.out.println(carOne.getName ());
+
+    carOne.setModel(2018)  ;
+    System.out.println(carOne.getModel () );
+
+    carOne.setColor ("Purple")  ;
+    System.out.println(carOne.getColor () );
+    
     }
-    }
+}
