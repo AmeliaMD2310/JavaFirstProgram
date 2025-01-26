13a479cf44df13cdc905dd3fbc0bbbb2f95e989a
AmeliaMD2310 <w0467922@nscc.ca>
2025-01-26 16:16:36 -0400

    Add files via upload
    Exercise 3

--- Car.java
+++ Car.java
@@ -1,31 +1,57 @@
 public class Car {
-    // add attributes
+    // Attributes
     private String name;
-    private int  model;
-    private  String color;
-    
-    
-    
-    // add behaviour  getters and setters (methods)
-        //getters
-        public String getName () {
-           return name; 
-        }
-    
-        public int getModel () {
-           return model; 
-        }
-        public String getColor () {
-           return color; 
-        }
-       // setters
-       public void setName (String n) {
-           this.name = n; 
-        }
-       public void setModel (int m) {
-          this.model = m; 
-        } 
-    public void setColor (String c) {
-            this.color = c;
-        } 
+    private String model;
+    private String color;
+    private int year;
+
+    // No-argument constructor (Default values)
+    public Car() {
+        this.name = "Unknown";
+        this.model = "Unknown";
+        this.color = "Unknown";
+        this.year = 0; 
     }
+
+    // Parameterized constructor (Custom Values)
+    public Car(String name, String model, String color, int year) {
+        this.name = name;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,28 +1,45 @@
 /*
 Amelia MacDonald
 W0467922
-January 15, 2025
-Exercise 2 
+January 26, 2025
+Object Oriented Programming
+Exercise 3
 */
+
 public class Main {
     public static void main(String[] args) {
-System.out.println("Hello, World!");
-System.out.println("I Love Cars!");
+        System.out.println("Hello, World!");
+        System.out.println("I Love Cars!");
 
-    Car carOne = new Car();
-    System.out.println(carOne.getName () );
+        // Using the no-argument constructor
+        Car carOne = new Car();
+        System.out.println("---No-Argument Constructor---");
+        System.out.println("Car One Details:");
+        System.out.println("Name: " + carOne.getName());
+        System.out.println("Model: " + carOne.getModel());
+        System.out.println("Color: " + carOne.getColor());
+        System.out.println("Year: " + carOne.getYear());
 
-    carOne.setName("Dodge Challenger");
-    System.out.println(carOne.getName () );
+        // Setting values using setters
+        carOne.setName("Dodge Challenger");
+        carOne.setModel("Hellcat");
+        carOne.setColor("Purple");
+        carOne.setYear(2023);
 
-    carOne.setName("Mitsubishi Lancer")  ;
-    System.out.println(carOne.getName ());
+        System.out.println("Updated Car One Details:");
+        System.out.println("Name: " + carOne.getName());
+        System.out.println("Model: " + carOne.getModel());
+        System.out.println("Color: " + carOne.getColor());
+        System.out.println("Year: " + carOne.getYear());
 
-    carOne.setModel(2018)  ;
-    System.out.println(carOne.getModel () );
+        // Using the parameterized constructor
+        Car carTwo = new Car("Mitsubishi Lancer", "SE", "Blue", 2016);
+        System.out.println("---Parameterized Constructor---");
+        System.out.println("Car Two Details:");
+        System.out.println("Name: " + carTwo.getName());
+        System.out.println("Model: " + carTwo.getModel());
+        System.out.println("Color: " + carTwo.getColor());
+        System.out.println("Year: " + carTwo.getYear());
 
-    carOne.setColor ("Purple")  ;
-    System.out.println(carOne.getColor () );
-    
     }
 }
